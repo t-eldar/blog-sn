c27c5fb6849c038ac8289a79963cfc77ada632ce
Danila.T <roman-petrov-05@inbox.ru>
2022-05-08 18:23:23 +0300

    Test

--- src/Test/api.js
+++ src/Test/api.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class api {
-    static URL = 'https://localhost:44386/api/Authenticate/register'
+    static URL = 'https://localhost:8080/api/Authenticate/'
 
     static async Login(userName, password){
-        const responce = await axios.post(URL, {userName, password})
+        const responce = await axios.post(api.URL + 'login', {userName, password})
         if(responce.data.token){
             localStorage.setItem('user', JSON.stringify(responce.data))
+            console.log('Welcome');
         }
         return responce;
     }
@@ -17,7 +18,7 @@ export default class api {
     }
 
     static async Register(userName, eMail, password){
-        const responce = await axios.post(api.URL, {userName, eMail, password})
+        const responce = await axios.post(api.URL + 'register', {userName, eMail, password})
         return responce;
     }
 
