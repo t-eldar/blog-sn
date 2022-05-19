2af406733a58554dfbf0774f73f6c7f866ef8e69
t-eldar <74774738+t-eldar@users.noreply.github.com>
2022-05-19 14:00:12 +0300

    Fixed validation

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -50,9 +50,9 @@ const RegisterForm = ({ onSuccess, isAdminRegister = false, style }) => {
 	]);
 	const [isUsernameInvalid, usernameErrorMessage] = useValidation(userInfo.username, [
 		(username) => {
-			const usernameRegEx = /^[A-Za-zА-Яа-я0-9]+$/;
+			const usernameRegEx = /^[A-Za-z0-9]+$/;
 			return {
-				message: 'Имя пользователя должно содержать только латинские или кириллические буквы или цифры',
+				message: 'Имя пользователя должно содержать только латинские буквы или цифры',
 				invalid: !usernameRegEx.test(username)
 			}
 		},
